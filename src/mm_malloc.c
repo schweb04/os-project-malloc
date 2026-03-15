3e530e3cc40d43a147dea11466433ad0ac4f5ab8
schweb04 <manelequintero@gmail.com>
2026-03-15 14:35:06 -0400

    Funciones 'my_malloc' (incluyendo la función auxiliar 'request_space'), 'my_free' (incluyendo la función auxiliar 'find_prev') y 'my_calloc' implementadas

--- src/mm_malloc.c
+++ src/mm_malloc.c
@@ -1,4 +1,6 @@
 #include <unistd.h> // Para sbrk
+#include <string.h> // Para memset
+#include <assert.h>
 #include "mm_malloc.h"
 
 // Inicio de la lista enlazada del heap
@@ -24,19 +26,91 @@ struct block_meta *find_free_block_best_fit(struct block_meta **last, size_t siz
     return best_fit;
 }
 
+struct block_meta *request_space(struct block_meta *last, size_t size) {
+    struct block_meta *block;
+    block = sbrk(0);
+    void *request = sbrk(size + META_SIZE);
+    assert((void*)block == request);
+    
+    if (request == (void*) -1) {
+        return NULL;
+    }
+
+    if (last) {
+        last->next = block;
+    }
+    block->size = size;
+    block->next = NULL;
+    block->free = 0;
+    block->magic = 0x00;
+    return block;
+}
+
+struct block_meta *find_prev(struct block_meta *block) {
+    struct block_meta *current = base;
+    struct block_meta *prev = NULL;
+
+    while (current && current != block) {
+        prev = current;
+        current = current->next;
+    }
+    return prev;
+}
+
 void *my_malloc(size_t size) {
     // TODO: Implementar First-Fit o Best-Fit
     // 1. Verificar si hay un bloque libre del tamaño adecuado.
     // 2. Si no, pedir espacio al OS con sbrk().
-    return NULL; 
+    struct block_meta* block;
+    
+    if (!base) {
+        block = request_space(NULL, size);
+
+        if (!block) {
+            return NULL;
+        }
+        base = block;
+    } else {
+        struct block_meta *last = base;
+        block = find_free_block_best_fit(&last, size);
+
+        if (!block) {
+            block = request_space(last, size);
+
+            if (!block) {
+                return NULL;
+            }
+        } else {
+            block->free = 0;
+            block->magic = 0x10;
+        }
+    }
+    return (block + 1); 
 }
 
 void my_free(void *ptr) {
     // TODO: Marcar el bloque como libre.
     // TODO: Fusionar bloques adyacentes (Coalescing).
-
-    if (ptr == NULL) {
+    if (!ptr) {
         return;
+    }
+    struct block_meta *block = get_block(ptr);
+    assert(block->free == 0);
+    assert(block->magic == 0x00 || block->magic == 0x10);
+    block->free = 1;
+    block->magic = 0x1;
+
+    struct block_meta *prev_block = find_prev(block);
+    struct block_meta *next_block = block->next;
+
+    if (next_block && next_block->free) {
+        block->size += META_SIZE + next_block->size;
+        block->next = next_block->next;
+    }
+    
+    if (prev_block && prev_block->free) {
+        prev_block->size += META_SIZE + block->size;
+        prev_block->next = block->next;
     }
 }
 
@@ -53,5 +127,8 @@ void *my_calloc(size_t nmemb, size_t size) {
 
 void *my_realloc(void *ptr, size_t size) {
     // TODO: Redimensionar el bloque o moverlo a uno nuevo.
+
+    (void)ptr;
+    (void)size;
     return NULL;
 }
